02024cb72eaef884733c1ffbfe5bcc7b21aa8ee3
Benedikt Werner <1benediktwerner@gmail.com>
2017-12-18 20:42:00 +0100

    day 18 part 2

--- day18/sol.py
+++ day18/sol.py
@@ -33,5 +33,55 @@ def last_recovered_sound(instructions):
                 continue
         bz += 1
 
+def part2(instructions):
+    def get(x):
+        try:
+            return int(x)
+        except ValueError:
+            return registers[program][x]
+
+    program = 0
+    bz = [0, 0]
+    length = len(instructions)
+    registers = [defaultdict(lambda: 0), defaultdict(lambda: 0)]
+    registers[1]["p"] = 1
+    queue = [[],[]]
+    result = 0
+    terminated = [False]*2
+    while True:
+        while 0 <= bz[program] < length:
+            instr = instructions[bz[program]].split(" ")
+            if instr[0] == "snd":
+                queue[(program+1)%2].append(get(instr[1]))
+                if program == 1:
+                    result += 1
+            elif instr[0] == "set":
+                registers[program][instr[1]] = get(instr[2])
+            elif instr[0] == "add":
+                registers[program][instr[1]] += get(instr[2])
+            elif instr[0] == "mul":
+                registers[program][instr[1]] *= get(instr[2])
+            elif instr[0] == "mod":
+                registers[program][instr[1]] %= get(instr[2])
+            elif instr[0] == "rcv":
+                if queue[program]:
+                    registers[program][instr[1]] = queue[program].pop(0)
+                elif terminated[(program+1)%2] or not queue[(program+1)%2]:
+                    return result
+                else:
+                    program = (program+1)%2
+                    continue
+            elif instr[0] == "jgz":
+                if get(instr[1]) > 0:
+                    bz[program] += get(instr[2])
+                    continue
+            bz[program] += 1
+        terminated[program] = True
+        if terminated[(program + 1)%2]:
+            return result
+        program = (program+1)%2
+
 with open("input.txt", "r") as f:
-    print(last_recovered_sound([line.strip() for line in f]))
+    instructions = [line.strip() for line in f]
+    print(last_recovered_sound(instructions))
+    print(part2(instructions))
