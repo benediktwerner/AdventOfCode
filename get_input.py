4fd5dbec87fb287e052408ed80d02e1626f52cd9
Benedikt Werner <1benediktwerner@gmail.com>
2018-12-08 13:46:48 +0100

    Add setup and get_input scripts

---
+++ get_input.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+from sys import argv
+
+
+YEAR = 2018
+URL = "https://adventofcode.com/{year}/day/{day}/input"
+
+
+if len(argv) != 2:
+    print("Usage:", argv[0], "DAY")
+    exit(1)
+
+try:
+    day = int(argv[1])
+except ValueError:
+    print("Error: DAY is not a number")
+    print("Usage:", argv[0], "DAY")
+    exit(1)
+
+
+dir_name = "day{:02}".format(day)
+cookies = {}
+
+with open("cookie") as f:
+    cookies["session"] = f.read().strip()
+
+with open(os.path.join(dir_name, "input.txt"), "w") as f:
+    url = URL.format(year=YEAR, day=argv[1])
+    req = requests.get(url, cookies=cookies)
+    f.write(req.text)
