9c7b18ff4009493de03278f4ec8af51fd4232cff
Benedikt Werner <1benediktwerner@gmail.com>
2019-11-29 00:05:24 +0100

    2015 Day 13 and 16

---
+++ 2015/day13/sol.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+
+
+def find_opt(m, names, curr=None):
+    if curr is None:
+        curr = []
+
+    if not names:
+        total = 0
+        for i, n in enumerate(curr):
+            total += m[n][curr[(i + 1) % len(curr)]]
+            total += m[n][curr[(i - 1) % len(curr)]]
+        return total
+
+    curr_max = float("-inf")
+
+    for n in tuple(names):
+        names.remove(n)
+        curr.append(n)
+        val = find_opt(m, names, curr)
+        if val > curr_max:
+            curr_max = val
+        curr.pop()
+        names.add(n)
+
+    return curr_max
+
+
+def main():
+    with open(__file__.rstrip("sol.py") + "input.txt") as f:
+        m = defaultdict(dict)
+
+        for line in f:
+            line = line.strip()
+            parts = line.split()
+            name = parts[0]
+            happiness = int(parts[3])
+            if parts[2] == "lose":
+                happiness *= -1
+            target = parts[-1][:-1]
+            m[name][target] = happiness
+
+        names = set(m.keys())
+        print("Part 1:", find_opt(m, names))
+
+        for n in names:
+            m[n]["me"] = 0
+            m["me"][n] = 0
+
+        print("Part 2:", find_opt(m, names, ["me"]))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2015/day16/sol.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+
+def main():
+    with open(__file__.rstrip("sol.py") + "input.txt") as f:
+        attrs = {
+            "children": 3,
+            "cats": 7,
+            "samoyeds": 2,
+            "pomeranians": 3,
+            "akitas": 0,
+            "vizslas": 0,
+            "goldfish": 5,
+            "trees": 3,
+            "cars": 2,
+            "perfumes": 1,
+        }
+
+        for line in f:
+            line = line.strip()
+
+            x = line.find(":")
+            aunt = int(line[4:x])
+            aunt_attrs = {
+                a.split(": ")[0]: int(a.split(": ")[1])
+                for a in line[x + 2 :].split(", ")
+            }
+
+            for k, v in attrs.items():
+                if k in aunt_attrs and aunt_attrs[k] != v:
+                    break
+            else:
+                print("Part 1:", aunt)
+
+            for k, v in attrs.items():
+                if k in aunt_attrs:
+                    if k in ("cats", "trees"):
+                        if aunt_attrs[k] <= v:
+                            break
+                    elif k in ("pomeranians", "goldfish"):
+                        if aunt_attrs[k] >= v:
+                            break
+                    elif aunt_attrs[k] != v:
+                        break
+            else:
+                print("Part 2:", aunt)
+
+
+if __name__ == "__main__":
+    main()
