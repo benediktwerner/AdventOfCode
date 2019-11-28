49c2c21c3baef278f1b64835f86a64f91c9abc8c
Benedikt Werner <1benediktwerner@gmail.com>
2019-11-28 22:19:47 +0100

    2016 Day 9

---
+++ 2016/day09/sol.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+
+def decompressed_length(line, start=0, end=None, recurse=True):
+    count = 0
+    i = start
+    if end is None:
+        end = len(line)
+
+    while i < end:
+        if line[i] == "(":
+            marker = ""
+            while True:
+                i += 1
+                if line[i] == ")":
+                    break
+                marker += line[i]
+            i += 1
+            length, repeat = map(int, marker.split("x"))
+            if recurse:
+                total_length = decompressed_length(line, i, i + length)
+            else:
+                total_length = length
+            count += total_length * repeat
+            i += length
+        else:
+            count += 1
+            i += 1
+
+    return count
+
+
+def main():
+    with open(__file__.rstrip("sol.py") + "input.txt") as f:
+        line = f.readline().strip()
+
+        print("Part 1:", decompressed_length(line, recurse=False))
+        print("Part 2:", decompressed_length(line))
+
+
+if __name__ == "__main__":
+    main()
