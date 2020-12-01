d62299643a539764204f6f8b244a35d5b58e9ac2
Benedikt Werner <1benediktwerner@gmail.com>
2020-12-01 07:10:54 +0100

    2020: Make day 1 nicer

--- 2020/day01/sol.py
+++ 2020/day01/sol.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 
 from os import path
+import itertools
+import operator
+import functools
 
 
 with open(path.join(path.dirname(__file__), "input.txt")) as f:
     numbers = [int(x) for x in f]
-    part1 = None
-    part2 = None
 
-    for i, a in enumerate(numbers):
-        for j, b in enumerate(numbers[i + 1 :], i + 1):
-            if a + b == 2020:
-                part1 = a * b
+    def solve(count):
+        for ns in itertools.combinations(numbers, count):
+            if sum(ns) == 2020:
+                return functools.reduce(operator.mul, ns)
 
-            for c in numbers[j + 1 :]:
-                if a + b + c == 2020:
-                    part2 = a * b * c
-
-    print("Part 1:", part1)
-    print("Part 2:", part2)
+    print("Part 1:", solve(2))
+    print("Part 2:", solve(3))
