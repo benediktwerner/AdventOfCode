1369215bdf73425b58341c88507a09b84b4b3a6d
Benedikt Werner <1benediktwerner@gmail.com>
2020-12-02 12:38:46 +0100

    2020/optimized: Add benchmark that includes parsing

--- 2020/optimized/src/days/day01.rs
+++ 2020/optimized/src/days/day01.rs
@@ -6,9 +6,9 @@ impl crate::Solver for Solver {
     type Input = Vec<u32>;
     type Output = (u32, u32);
 
-    fn parse_input(&self, input: &[String]) -> anyhow::Result<Self::Input> {
+    fn parse_input(&self, input: &str) -> anyhow::Result<Self::Input> {
         input
-            .iter()
+            .lines()
             .map(|s| {
                 s.parse::<u32>()
                     .with_context(|| format!("Failed to convert {} to int", s))
@@ -50,6 +50,61 @@ impl crate::Solver for Solver {
             }
 
             for &b in input.get_unchecked(i + 1..) {
+                if b > opp {
+                    break;
+                }
+                let opp2 = opp - b;
+                if *nums.get_unchecked(opp2 as usize) {
+                    output.1 = a * b * opp2;
+                    if output.0 != 0 {
+                        return output;
+                    }
+                }
+            }
+        }
+
+        output
+    }
+
+    unsafe fn solve_str(&self, input: &str) -> Self::Output {
+        let mut output = (0, 0);
+
+        let mut nums = [false; 2020];
+
+        let mut num = 0;
+        let mut count = 0;
+        for &c in input.as_bytes() {
+            if c == b'\n' {
+                *nums.get_unchecked_mut(num) = true;
+                num = 0;
+                count += 1;
+            } else {
+                num = num * 10 + (c - b'0') as usize;
+            }
+        }
+
+        let mut nums_list = Vec::with_capacity(count);
+        let mut i = 0;
+        nums_list.set_len(count);
+        for (n, b) in nums.iter().enumerate() {
+            if *b {
+                *nums_list.get_unchecked_mut(i) = n as u32;
+                i += 1;
+            }
+        }
+
+        debug_assert!(i == count);
+
+        for (i, &a) in nums_list.iter().enumerate() {
+            let opp = 2020 - a;
+            if *nums.get_unchecked(opp as usize) {
+                output.0 = a * opp;
+                if output.1 != 0 {
+                    return output;
+                }
+            }
+
+            for &b in nums_list.get_unchecked(i + 1..) {
                 if b > opp {
                     break;
                 }
--- 2020/optimized/src/main.rs
+++ 2020/optimized/src/main.rs
@@ -1,26 +1,40 @@
-use std::time::Instant;
-
-use input::read_input;
+use rand::seq::SliceRandom as _;
+use std::{path::Path, time::Instant};
 
 mod days;
-mod input;
 mod utils;
 
 trait Solver {
     type Input;
     type Output;
 
-    fn parse_input(&self, input: &[String]) -> anyhow::Result<Self::Input>;
+    fn parse_input(&self, input: &str) -> anyhow::Result<Self::Input>;
     fn is_input_safe(&self, input: &Self::Input) -> bool;
     unsafe fn solve(&self, input: Self::Input) -> Self::Output;
+    unsafe fn solve_str(&self, input: &str) -> Self::Output;
+}
+
+fn input_path(day: u8) -> std::path::PathBuf {
+    Path::new("..")
+        .join(format!("day{:02}", day))
+        .join("input.txt")
 }
 
 fn main() {
     let solver = days::day01::Solver;
-    let input = match solver.parse_input(&read_input(1)) {
+
+    let path = input_path(1);
+    let input_string = match std::fs::read_to_string(path) {
         Ok(inp) => inp,
         Err(error) => {
-            println!("Error while parsing input: {}", error);
+            eprintln!("Error while reading input file: {}", error);
+            return;
+        }
+    };
+    let input = match solver.parse_input(&input_string) {
+        Ok(inp) => inp,
+        Err(error) => {
+            eprintln!("Error while parsing input file: {}", error);
             return;
         }
     };
@@ -30,7 +44,6 @@ fn main() {
     let mut rng = rand::thread_rng();
 
     for _ in 0..COUNT {
-        use rand::seq::SliceRandom;
         let mut inp = input.clone();
         inp.shuffle(&mut rng);
         assert!(solver.is_input_safe(&inp), "Input unsafe");
@@ -43,7 +56,16 @@ fn main() {
 
     println!("Time: {}", utils::format_duration(sum / COUNT));
 
-    // println!("Part 1: {}", out.0);
-    // println!("Part 2: {}", out.1);
-    // println!("Time: {}", utils::format_duration(end.duration_since(start).as_nanos()));
+    sum = 0;
+
+    for _ in 0..COUNT {
+        let inp = &input_string;
+        let start = Instant::now();
+        let out = unsafe { solver.solve_str(inp) };
+        let end = Instant::now();
+        assert_eq!(out, (1015476, 200878544), "Wrong result");
+        sum += end.duration_since(start).as_nanos();
+    }
+
+    println!("Time: {}", utils::format_duration(sum / COUNT));
 }
