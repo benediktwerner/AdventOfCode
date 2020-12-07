445999437f3607af5440428ebaff9dc2557adad6
Benedikt Werner <1benediktwerner@gmail.com>
2020-12-07 01:19:46 +0100

    2020/optimized/day05: SIMD 2

--- 2020/optimized/src/days/day05.rs
+++ 2020/optimized/src/days/day05.rs
@@ -22,6 +22,7 @@ impl crate::Solver for Solver {
             input.len() % 11 == 0,
             "Input length is not a multiple of 11"
         );
+        ensure!(input.len() % 44 == 0, "Input length is not a multiple of 44");
         ensure!(input.is_ascii(), "Input contains non-ASCII characters");
         for i in 0..input.len() / 11 {
             ensure!(
@@ -48,35 +49,27 @@ impl crate::Solver for Solver {
     }
 
     unsafe fn solve(&self, input: &str) -> (String, String) {
-        use core::arch::x86_64::__m128i;
-        use core::arch::x86_64::_mm_loadu_si128;
-        use core::arch::x86_64::_mm_shuffle_epi8;
-        use core::arch::x86_64::_mm_andnot_si128;
-        use core::arch::x86_64::_mm_slli_epi64;
-        use core::arch::x86_64::_mm_movemask_epi8;
-
         let mut min: u32 = u32::MAX;
         let mut max: u32 = 0;
         let mut sum: u32 = 0;
-
-        const SHUFFLE: __m128i = unsafe { std::mem::transmute(0xff_ff_ff_ff_ff_ff_00_01_02_03_04_05_06_07_08_09_u128) };
-        const MASK: __m128i = unsafe { std::mem::transmute(0x0404_0404_0404_0404_0404_u128) };
 
         let mut ptr = input.as_bytes().as_ptr();
         let end_ptr = ptr.add(input.len());
 
         while ptr < end_ptr {
-            let mut ticket_bytes = _mm_loadu_si128(ptr as *const __m128i);
-            ticket_bytes = _mm_shuffle_epi8(ticket_bytes, SHUFFLE);
-            ticket_bytes = _mm_andnot_si128(ticket_bytes, MASK);
-            ticket_bytes = _mm_slli_epi64(ticket_bytes, 5);
-            let ticket_id = _mm_movemask_epi8(ticket_bytes) as u32;
-
-            sum += ticket_id;
-            min = min.min(ticket_id);
-            max = max.max(ticket_id);
+            let (a,b) = parse2(ptr);
+            sum += a + b;
+            min = min.min(a).min(b);
+            max = max.max(a).max(b);
 
             ptr = ptr.add(11);
+
+            let (a,b) = parse2(ptr);
+            sum += a + b;
+            min = min.min(a).min(b);
+            max = max.max(a).max(b);
+
+            ptr = ptr.add(33);
         }
 
         // sum(min..=max)
@@ -85,4 +78,32 @@ impl crate::Solver for Solver {
 
         (max.to_string(), part2.to_string())
     }
+}
+
+#[inline(always)]
+unsafe fn parse2(ptr: *const u8) -> (u32, u32) {
+    use core::arch::x86_64::__m256i;
+    use core::arch::x86_64::_mm256_andnot_si256;
+    use core::arch::x86_64::_mm256_loadu_si256;
+    use core::arch::x86_64::_mm256_movemask_epi8;
+    use core::arch::x86_64::_mm256_set_epi8;
+    use core::arch::x86_64::_mm256_shuffle_epi8;
+    use core::arch::x86_64::_mm256_slli_epi64;
+
+    let shuffle = _mm256_set_epi8(
+        -1, -1, -1, -1, -1, -1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, //
+        -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, //
+    );
+    let mask = _mm256_set_epi8(
+        0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, //
+        0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, //
+    );
+
+    let mut ticket_bytes = _mm256_loadu_si256(ptr as *const __m256i);
+    ticket_bytes = _mm256_shuffle_epi8(ticket_bytes, shuffle);
+    ticket_bytes = _mm256_andnot_si256(ticket_bytes, mask);
+    ticket_bytes = _mm256_slli_epi64(ticket_bytes, 5);
+    let ticket_id = _mm256_movemask_epi8(ticket_bytes) as u32;
+
+    (ticket_id & 0xffff, ticket_id >> 16)
 }
