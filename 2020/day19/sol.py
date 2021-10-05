2bf0e89d125bfac9d1417b7f3456a5e7970cd1eb
Benedikt Werner <1benediktwerner@gmail.com>
2021-10-05 11:55:04 +0200

    2020: Day 19/2

--- 2020/day19/sol.py
+++ 2020/day19/sol.py
@@ -6,15 +6,17 @@ import re
 
 def build_re(rule):
     if "|" in rule:
-        a, b = rule.split(" | ")
-        return f"(?:(?:{build_re(a)})|(?:{build_re(b)}))"
+        parts = rule.split(" | ")
+        return "(?:" + "|".join(f"(?:{build_re(x)})" for x in parts) + ")"
     if rule[0] == '"':
         return rule[1]
+    if rule[-1] == "+":
+        return build_re(rules[rule[:-1]]) + "+"
     return "".join(build_re(rules[r]) for r in rule.split())
 
 
 with open(path.join(path.dirname(__file__), "input.txt")) as f:
-    lines = map(lambda line: line.strip(), f)
+    lines = (line.strip() for line in f)
     rules = {}
     for line in lines:
         if not line:
@@ -22,11 +24,22 @@ with open(path.join(path.dirname(__file__), "input.txt")) as f:
         num, rule = line.split(": ")
         rules[num] = rule
 
-    regex = build_re(rules["0"])
+    regex1 = build_re(rules["0"])
 
-    count = 0
-    for line in lines:
-        if re.fullmatch(regex, line):
-            count += 1
+    # rules["8"] = "42 | 42 8"
+    # rules["11"] = "42 31 | 42 11 31"
+
+    rules["8"] = "42+"
+    rules["11"] = "42 31 | 42 42 31 31 | 42 42 42 31 31 31 | 42 42 42 42 31 31 31 31 | 42 42 42 42 42 31 31 31 31 31 | 42 42 42 42 42 42 31 31 31 31 31 31 | 42 42 42 42 42 42 42 31 31 31 31 31 31 31 | 42 42 42 42 42 42 42 42 31 31 31 31 31 31 31 31"
     
-    print("Part 1:", count)
+    regex2 = build_re(rules["0"])
+
+    part1 = part2 = 0
+    for line in lines:
+        if re.fullmatch(regex1, line):
+            part1 += 1
+        if re.fullmatch(regex2, line):
+            part2 += 1
+    
+    print("Part 1:", part1)
+    print("Part 2:", part2)
