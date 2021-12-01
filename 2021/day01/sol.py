6103730e5ce54d616105ae504b8e2725b574b797
Benedikt Werner <1benediktwerner@gmail.com>
2021-12-01 13:24:52 +0100

    2021: Day 01 improved

--- 2021/day01/sol.py
+++ 2021/day01/sol.py
@@ -3,17 +3,7 @@
 from os import path
 
 
-def sliding_window(coll, n):
-    for i in range(len(coll) - n + 1):
-        yield coll[i : i + n]
-
-
-def solve(depths, n):
-    windows = list(map(sum, sliding_window(depths, n)))
-    return sum(b > a for a, b in zip(windows, windows[1:]))
-
-
 with open(path.join(path.dirname(__file__), "input.txt")) as f:
     depths = [int(x) for x in f.read().splitlines()]
-    print("Part 1:", solve(depths, 1))
-    print("Part 2:", solve(depths, 3))
+    print("Part 1:", sum(b > a for a, b in zip(depths, depths[1:])))
+    print("Part 2:", sum(b > a for a, b in zip(depths, depths[3:])))
