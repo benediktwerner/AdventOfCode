3f283ef66f64e8f90200fef54893d0cd188af4c1
Benedikt Werner <1benediktwerner@gmail.com>
2021-12-22 10:57:52 +0100

    2021: Day 22 nice

--- 2021/day22/sol.py
+++ 2021/day22/sol.py
@@ -1,126 +1,62 @@
 #!/usr/bin/env python3
 
-from __future__ import annotations
-
 from os import path
 import re
+from collections import Counter
 from dataclasses import dataclass
-from typing import List, Optional, Tuple, Iterator
-
-
-def ints(string):
-    return map(int, re.findall(r"-?[0-9]+", string))
-
-
-@dataclass(frozen=True)
-class Line:
-    start: int
-    end: int
-
-    def cut_off(self, other: Line) -> List[Tuple[bool, Line]]:
-        if other.end < self.start or self.end < other.start:
-            return [(True, self)]
-        elif other.start <= self.start and self.end <= other.end:
-            return [(False, self)]
-        elif other.start <= self.start and other.end < self.end:
-            return [
-                (False, Line(self.start, other.end)),
-                (True, Line(other.end + 1, self.end)),
-            ]
-        elif self.start < other.start and self.end <= other.end:
-            return [
-                (True, Line(self.start, other.start - 1)),
-                (False, Line(other.start, self.end)),
-            ]
-        else:
-            return [
-                (True, Line(self.start, other.start - 1)),
-                (False, other),
-                (True, Line(other.end + 1, self.end)),
-            ]
-
-    def intersect(self, other: Line) -> Optional[Line]:
-        start = max(self.start, other.start)
-        end = min(self.end, other.end)
-        if start <= end:
-            return Line(start, end)
-
-    @property
-    def length(self) -> int:
-        return max(self.end - self.start + 1, 0)
-
-    def __iter__(self) -> Iterator[int]:
-        return iter(range(self.start, self.end + 1))
 
 
 @dataclass(frozen=True)
 class Cube:
-    x: Line
-    y: Line
-    z: Line
+    x1: int
+    x2: int
+    y1: int
+    y2: int
+    z1: int
+    z2: int
 
-    def cut_off(self, other: Cube) -> Iterator[Cube]:
-        for x, xl in self.x.cut_off(other.x):
-            for y, yl in self.y.cut_off(other.y):
-                for z, zl in self.z.cut_off(other.z):
-                    if x or y or z:
-                        yield Cube(xl, yl, zl)
+    @staticmethod
+    def from_line(line):
+        return Cube(*map(int, re.findall(r"-?[0-9]+", line)))
 
-    def intersect(self, other: Cube) -> Optional[Cube]:
-        x = self.x.intersect(other.x)
-        y = self.y.intersect(other.y)
-        z = self.z.intersect(other.z)
-        if x and y and z:
-            return Cube(x, y, z)
+    def intersect(self, other):
+        c = Cube(
+            max(self.x1, other.x1),
+            min(self.x2, other.x2),
+            max(self.y1, other.y1),
+            min(self.y2, other.y2),
+            max(self.z1, other.z1),
+            min(self.z2, other.z2),
+        )
+        if c.x1 <= c.x2 and c.y1 <= c.y2 and c.z1 <= c.z2:
+            return c
 
-    @property
-    def volume(self) -> int:
-        return self.x.length * self.y.length * self.z.length
+    def volume(self):
+        return (
+            (self.x2 - self.x1 + 1) * (self.y2 - self.y1 + 1) * (self.z2 - self.z1 + 1)
+        )
 
 
-def part1(ops: List[Tuple[bool, Cube]]) -> int:
-    cubes = set()
-    area = Cube(*([Line(-50, 50)] * 3))
+def solve(ops, window=None):
+    cubes = Counter()
     for on, c in ops:
-        if cube := c.intersect(area):
-            for x in cube.x:
-                for y in cube.y:
-                    for z in cube.z:
-                        coord = (x, y, z)
-                        if on:
-                            cubes.add(coord)
-                        elif coord in cubes:
-                            cubes.remove(coord)
-    return len(cubes)
+        if window and (c := c.intersect(window)) is None:
+            continue
+        new = Counter()
+        for cc, count in cubes.items():
+            if inter := c.intersect(cc):
+                new[inter] -= count
+        cubes.update(new)
 
-
-def part2(ops: List[Tuple[bool, Cube]]) -> int:
-    volume = 0
-    for i, (on, c) in enumerate(ops):
-        cs = [c] if on else []
-        for onn, cube in ops[:i]:
-            if on == onn:
-                new_cs = []
-                for g in cs:
-                    new_cs.extend(g.cut_off(cube))
-                cs = new_cs
-            elif cube := c.intersect(cube):
-                new_cs = [cube]
-                for g in cs:
-                    new_cs.extend(g.cut_off(cube))
-                cs = new_cs
-        volume += sum(c.volume for c in cs) * (-1, 1)[on]
-
-    return volume
+        if on:
+            cubes[c] += 1
+    return sum(c.volume() * count for c, count in cubes.items())
 
 
 with open(path.join(path.dirname(__file__), "input.txt")) as f:
-    ops = []
-    for line in f.read().splitlines():
-        on = line.split()[0] == "on"
-        x1, x2, y1, y2, z1, z2 = ints(line)
-        c = Cube(Line(x1, x2), Line(y1, y2), Line(z1, z2))
-        ops.append((on, c))
-
-    print("Part 1:", part1(ops))
-    print("Part 2:", part2(ops))
+    ops = [
+        (line.split()[0] == "on", Cube.from_line(line))
+        for line in f.read().splitlines()
+    ]
+    print("Part 1:", solve(ops, Cube(-50, 50, -50, 50, -50, 50)))
+    print("Part 2:", solve(ops))
