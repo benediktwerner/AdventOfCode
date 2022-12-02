a4d90605c6d0a019b42dc84fbcada2ce4c6a848c
Benedikt Werner <1benediktwerner@gmail.com>
2022-12-02 22:07:36 +0100

    2022: Day 02

---
+++ 2022/day02/sol.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from os import path
+
+
+def score(mine, theirs):
+    return [3, 6, 0][(mine - theirs) % 3] + mine + 1
+
+
+with open(path.join(path.dirname(__file__), "input.txt")) as f:
+    score1, score2 = 0, 0
+
+    for line in f.read().splitlines():
+        theirs, mine = map(ord, line.split())
+        theirs -= ord("A")
+        mine -= ord("X")
+
+        score1 += score(mine, theirs)
+        score2 += score((theirs + mine - 1) % 3, theirs)
+
+    print("Part 1:", score1)
+    print("Part 2:", score2)
