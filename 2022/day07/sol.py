98c8c6ada5151c31e6194b93f3cdf799e65c5b56
Benedikt Werner <1benediktwerner@gmail.com>
2022-12-07 07:21:46 +0100

    2022: Day 07 small adjustment

--- 2022/day07/sol.py
+++ 2022/day07/sol.py
@@ -6,31 +6,27 @@ from collections import defaultdict
 
 with open(path.join(path.dirname(__file__), "input.txt")) as f:
     dirs = defaultdict(int)
-    ls = False
+    cwd = []
     for line in f.read().splitlines():
         if line.startswith("$ cd"):
-            ls = False
             d = line[5:]
-            if d == "/":
-                cwd = []
-            elif d == "..":
+            if d == "..":
                 cwd.pop()
             else:
                 cwd.append(d)
         elif line.startswith("$ ls"):
-            ls = True
-        elif ls:
+            continue
+        else:
             try:
                 dirs["/".join(cwd)] += int(line.split()[0])
             except ValueError:
                 pass
 
 for d in sorted(dirs.keys(), key=lambda x: x.count("/"), reverse=True):
-    if d:
-        dirs["/".join(d.split("/")[:-1])] += dirs[d]
+    dirs["/".join(d.split("/")[:-1])] += dirs[d]
 
 print("Part 1:", sum(s for s in dirs.values() if s <= 100_000))
 
-free = 70_000_000 - dirs[""]
+free = 70_000_000 - dirs["/"]
 needed = 30_000_000 - free
 print("Part 2:", min(v for v in dirs.values() if v > needed))
