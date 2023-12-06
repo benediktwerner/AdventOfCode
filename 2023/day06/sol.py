99efb269dcf0411b2d0c12eb30de321757528552
Benedikt Werner <1benediktwerner@gmail.com>
2023-12-06 06:43:19 +0100

    2023: Day 06 nicer solution

--- 2023/day06/sol.py
+++ 2023/day06/sol.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 from os import path
+import math
 import re
-import pyperclip
 
 
 def ints(string) -> list[int]:
@@ -13,7 +13,21 @@ def solve(inp: str):
     result = 1
     times, dists = map(ints, inp.splitlines())
     for t, d in zip(times, dists):
-        result *= sum((t - hold_t) * hold_t > d for hold_t in range(t))
+        # brute-force solution
+        # result *= sum((t - hold_t) * hold_t > d for hold_t in range(t))
+
+        # mathematical solution. calculate the roots of the quadratic equation. the solution is the range they span.
+        # d == (t - hold_t) * hold_t
+        # 0 == (t - hold_t) * hold_t - d
+        # 0 == t * hold_t - hold_t^2 - d
+        # 0 == -hold_t^2 + t * hold_t - d
+        # hold_t = (-t +- sqrt(t^2 - 4 * (-1) * (-d))) / -2
+        sqrt = math.sqrt(t * t - 4 * d)
+        a = (-t - sqrt) / -2
+        b = (-t + sqrt) / -2
+        a, b = math.floor(min(a, b) + 1), math.ceil(max(a, b) - 1)
+        result *= b - a + 1
+
     return result
 
 
