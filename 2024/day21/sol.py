846125e5f91d9c900d758b1d9a14aee653f5cd7d
Benedikt Werner <1benediktwerner@gmail.com>
2024-12-21 12:03:04 +0100

    2024: Day 21

--- 2024/day21/sol.py
+++ 2024/day21/sol.py
@@ -1,28 +1,9 @@
 #!/usr/bin/env python3
 
-from __future__ import annotations
-
-from dataclasses import dataclass
 import itertools
-import math
-import operator
-import re
-from collections import Counter, defaultdict, deque
-from copy import deepcopy
 from functools import cache
 from os import path
-
-import pyperclip
-
-
-@dataclass(frozen=True)
-class Pos:
-    x: int
-    y: int
-
-    def move_by(self, dir: int) -> Pos:
-        dx, dy = DIR_TO_DELTA[dir]
-        return Pos(self.x + dx, self.y + dy)
+from typing import Iterable
 
 
 RIGHT, DOWN, LEFT, UP, ACTIVATE = range(5)
@@ -32,108 +13,99 @@ DIR_TO_DELTA = {
     LEFT: (-1, 0),
     UP: (0, -1),
 }
-ALLOWED_DIRS_ROBO = {
-    Pos(1, 0): [RIGHT, DOWN],
-    Pos(2, 0): [LEFT, DOWN],
-    Pos(0, 1): [RIGHT],
-    Pos(1, 1): [LEFT, UP, RIGHT],
-    Pos(2, 1): [LEFT, UP],
+ROBO_BUTTON_TO_POS = {
+    UP: (1, 0),
+    ACTIVATE: (2, 0),
+    LEFT: (0, 1),
+    DOWN: (1, 1),
+    RIGHT: (2, 1),
 }
-BUTTON_ROBO = {
-    Pos(1, 0): UP,
-    Pos(2, 0): ACTIVATE,
-    Pos(0, 1): LEFT,
-    Pos(1, 1): DOWN,
-    Pos(2, 1): RIGHT,
-}
-ALLOWED_DIRS_DOOR = {
-    Pos(0, 0): [DOWN, RIGHT],
-    Pos(1, 0): [LEFT, DOWN, RIGHT],
-    Pos(2, 0): [LEFT, DOWN],
-    Pos(0, 1): [UP, DOWN, RIGHT],
-    Pos(1, 1): [UP, LEFT, DOWN, RIGHT],
-    Pos(2, 1): [UP, LEFT, DOWN],
-    Pos(0, 2): [UP, RIGHT],
-    Pos(1, 2): [UP, LEFT, DOWN, RIGHT],
-    Pos(2, 2): [UP, LEFT, DOWN],
-    Pos(1, 3): [UP, RIGHT],
-    Pos(2, 3): [UP, LEFT],
-}
-BUTTON_DOOR = {
-    Pos(0, 0): "7",
-    Pos(1, 0): "8",
-    Pos(2, 0): "9",
-    Pos(0, 1): "4",
-    Pos(1, 1): "5",
-    Pos(2, 1): "6",
-    Pos(0, 2): "1",
-    Pos(1, 2): "2",
-    Pos(2, 2): "3",
-    Pos(1, 3): "0",
-    Pos(2, 3): "A",
+DOOR_BUTTON_TO_POS = {
+    "7": (0, 0),
+    "8": (1, 0),
+    "9": (2, 0),
+    "4": (0, 1),
+    "5": (1, 1),
+    "6": (2, 1),
+    "1": (0, 2),
+    "2": (1, 2),
+    "3": (2, 2),
+    "0": (1, 3),
+    "A": (2, 3),
 }
 
 
-def solve_code(code: str, robots) -> int:
-    def activate(i: int) -> int | None:
-        curr = positions[i]
-        next = positions[i - 1]
-        if BUTTON_ROBO[curr] != ACTIVATE and BUTTON_ROBO[curr] not in (
-            ALLOWED_DIRS_ROBO[next] if i > 1 else ALLOWED_DIRS_DOOR[next]
-        ):
-            return
-        if BUTTON_ROBO[curr] == ACTIVATE:
-            if i > 1:
-                return activate(i - 1)
-            elif BUTTON_DOOR[next] == code[code_index]:
-                if code_index + 1 == len(code):
-                    return presses + 1
-                if (code_index + 1, positions) not in seen:
-                    seen.add((code_index + 1, positions))
-                    todo.append((presses + 1, code_index + 1, positions))
-        else:
-            new_positions = (
-                positions[: i - 1] + (next.move_by(BUTTON_ROBO[curr]),) + positions[i:]
-            )
-            if (code_index, new_positions) not in seen:
-                seen.add((code_index, new_positions))
-                todo.append((presses + 1, code_index, new_positions))
+def paths_from_to(
+    sx: int, sy: int, ex: int, ey: int, on_door: bool
+) -> Iterable[tuple[int]]:
+    dx, dy = ex - sx, ey - sy
 
-    todo = deque([(0, 0, (Pos(2, 3),) + tuple(Pos(2, 0) for _ in range(robots)))])
-    seen = set([todo[0][1:]])
-    while todo:
-        presses, code_index, positions = todo.popleft()
-        for d in ALLOWED_DIRS_ROBO[positions[-1]]:
-            new_positions = positions[:-1] + (positions[-1].move_by(d),)
-            if (code_index, new_positions) not in seen:
-                seen.add((code_index, new_positions))
-                todo.append((presses + 1, code_index, new_positions))
-        if result := activate(robots):
-            return result
+    moves = []
+    if dx > 0:
+        moves += [RIGHT] * dx
+    elif dx < 0:
+        moves += [LEFT] * -dx
+    if dy > 0:
+        moves += [DOWN] * dy
+    elif dy < 0:
+        moves += [UP] * -dy
 
-    assert False
+    if is_valid_path(sx, sy, moves, on_door):
+        yield tuple(moves)
+
+    moves.reverse()
+    if is_valid_path(sx, sy, moves, on_door):
+        yield tuple(moves)
 
 
-def solve(inp: str):
-    result = 0
+def is_valid_path(x: int, y: int, moves: list[int], on_door: bool) -> bool:
+    for m in moves:
+        dx, dy = DIR_TO_DELTA[m]
+        x, y = x + dx, y + dy
+        if on_door:
+            if x == 0 and y == 3:
+                return False
+        elif x == 0 and y == 0:
+            return False
+    return True
 
-    for code in inp.splitlines():
-        result += solve_code(code, 25) * int(code[:-1])
 
-    return result
+@cache
+def presses_to_move_and_then_activate(moves: tuple[int], level: int) -> int:
+    if level == 0:
+        return len(moves) + 1
+
+    presses = 0
+    x, y = ROBO_BUTTON_TO_POS[ACTIVATE]
+    for b in itertools.chain(moves, [ACTIVATE]):
+        tx, ty = ROBO_BUTTON_TO_POS[b]
+        presses += min(
+            presses_to_move_and_then_activate(moves, level - 1)
+            for moves in paths_from_to(x, y, tx, ty, False)
+        )
+        x, y = tx, ty
+    return presses
 
 
-example = """\
+def code_complexity(code: str, levels: int) -> int:
+    presses = 0
+    x, y = DOOR_BUTTON_TO_POS["A"]
+    for b in code:
+        tx, ty = DOOR_BUTTON_TO_POS[b]
+        presses += min(
+            presses_to_move_and_then_activate(moves, levels)
+            for moves in paths_from_to(x, y, tx, ty, True)
+        )
+        x, y = tx, ty
+    return presses * int(code[:-1])
 
-"""
-
-if example and not example.isspace():
-    print("Example:", solve(example))
-else:
-    print("No example provided")
 
 with open(path.join(path.dirname(__file__), "input.txt")) as file:
-    result = solve(file.read())
-    print("Output:", result)
-    pyperclip.copy(str(result))
-    print("Copied to clipboard")
+    part1 = part2 = 0
+
+    for code in file.read().splitlines():
+        part1 += code_complexity(code, 2)
+        part2 += code_complexity(code, 25)
+
+    print("Part 1:", part1)
+    print("Part 2:", part2)
